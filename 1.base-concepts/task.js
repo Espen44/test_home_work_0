18b3798c8791012dca9cf2646df26517c7c07119
Espen <espen@espen.com>
2022-07-10 20:28:30 +0300

    сделал 2 задание в task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -10,7 +10,7 @@ function solveEquation(a, b, c) {
     arr.push(-b/(2*a));
   } else  {
     arr=[];
-    arr.push((-b + Math.sqrt(d))/(2*a), (-b - Math.sqrt(d))/(2*a) );
+    arr.push((-b + Math.sqrt(d))/(2*a), (-b - Math.sqrt(d))/(2*a));
   };
   // код для задачи №1 писать здесь
   return arr; // array
@@ -19,7 +19,38 @@ function solveEquation(a, b, c) {
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  // код для задачи №2 писать здесь
+  if (isNaN(percent)){
+    return (`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`);
+  }
+  if (isNaN(contribution)){
+    return (`Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`);
+  }
+  if (isNaN(amount)){
+    return (`Параметр "Общая стоимость" содержит неправильное значение "${amount}"`);
+  }
 
-  return totalAmount;
+  let debtAmount = amount - contribution;
+  console.log(debtAmount);
+  let today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth();
+
+  
+  // let endDate = date;
+  // console.log(date);
+  let yearEnd = date.getFullYear();
+  console.log(yearEnd);
+  let monthEnd = date.getMonth();
+  console.log(monthEnd);
+
+  let dateAll = ((yearEnd - year)*12) + (monthEnd - month);
+  console.log(dateAll);
+
+  let p = 1/12 * percent / 100;
+  let monthPayment = debtAmount * ( p  + ( p  / (((1 + p) ** dateAll) - 1)));
+  console.log(monthPayment);
+
+  totalAmount = (monthPayment * dateAll).toFixed(2);
+  console.log(+totalAmount);
+  return +totalAmount;
 }
