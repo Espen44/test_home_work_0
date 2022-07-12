d38f036b45a9d8d72cbbd2d6050ac821dbabeebe
Espen <espen@espen.com>
2022-07-12 07:50:35 +0300

    сделал 1 задание

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,18 +1,32 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
+  min = arr[0];
+  max = arr[0];
+  sum = 0;
 
-  // Ваш код
-
+  for (let i = 0; i < arr.length; i++){
+    if(arr[i] > max ){
+        max = arr[i] ;
+    }
+    if (arr[i] < min) {
+      min = arr[i]
+    } 
+    sum += arr[i];
+    avg = +(sum / arr.length).toFixed(2);
+}
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
   let sum;
-
-  // Ваш код
-
+  for (let i = 0; i < arr.length; i++){
+    for (let k = 0; k < arr[i].length; k++){
+      sum += arr[i][k];
+    }   
+  }
+  console.log(sum);
   return sum;
 }
 
