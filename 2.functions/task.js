462f03da9a611f7e99e9114c53161d608f895633
Espen <espen@espen.com>
2022-07-12 18:20:17 +0300

    доделал 2 и 3 задание в task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
@@ -20,26 +22,47 @@ function getArrayParams(arr) {
 
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
   for (let i = 0; i < arr.length; i++){
-    for (let k = 0; k < arr[i].length; k++){
-      sum += arr[i][k];
+      sum += arr[i];
     }   
-  }
-  console.log(sum);
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+function makeWork(arrOfArr, worker) {
+  let max = worker(arrOfArr[0]);
+  for ( let i = 0; i < arrOfArr.length; i++){
+    if (worker(arrOfArr[i]) > max){
+      max = worker(arrOfArr[i]);
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let numMax = arr[0];
+  let numMin = arr[0];
+  let diff;
+  for (let i = 0; i < arr.length; i++){
+    if(arr[i] > numMax ){
+        numMax = arr[i] ;
+    }
+    if (arr[i] < numMin) {
+      numMin = arr[i]
+    } 
+    diff = numMax - numMin;
+}
+    return Math.abs(diff);
+}
+
+function makeWork(arrOfArr, worker2) {
+  let max = worker2(arrOfArr[0]);
+
+  for ( let i = 0; i < arrOfArr.length; i++){
+    if (worker2(arrOfArr[i]) > max){
+      max = worker2(arrOfArr[i]);
+    }
+  }
+  return max;
 }
