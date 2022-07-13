99ce1d07b4c6d047f49c33958979281e9b22de06
Anton Savelev <maybesomedayilearn@gmail.com>
2022-07-13 09:58:32 +0300

    Работа над ошибками в файле task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -15,8 +15,8 @@ function getArrayParams(arr) {
       min = arr[i]
     } 
     sum += arr[i];
-    avg = +(sum / arr.length).toFixed(2);
 }
+  avg = +(sum / arr.length).toFixed(2);
   return { min: min, max: max, avg: avg };
 }
 
@@ -29,13 +29,14 @@ function worker(arr) {
   return sum;
 }
 
-function makeWork(arrOfArr, worker) {
-  let max = worker(arrOfArr[0]);
+function makeWork(arrOfArr, func) {
+  let max = func(arrOfArr[0]); 
   for ( let i = 0; i < arrOfArr.length; i++){
-    if (worker(arrOfArr[i]) > max){
-      max = worker(arrOfArr[i]);
+      const funcArr = func(arrOfArr[i]);
+        if (funcArr > max){
+          max = funcArr;
+        }
     }
-  }
   return max;
 }
 
@@ -43,26 +44,13 @@ function makeWork(arrOfArr, worker) {
 function worker2(arr) {
   let numMax = arr[0];
   let numMin = arr[0];
-  let diff;
   for (let i = 0; i < arr.length; i++){
     if(arr[i] > numMax ){
-        numMax = arr[i] ;
+        numMax = arr[i];
     }
     if (arr[i] < numMin) {
-      numMin = arr[i]
+      numMin = arr[i];
     } 
-    diff = numMax - numMin;
 }
-    return Math.abs(diff);
-}
-
-function makeWork(arrOfArr, worker2) {
-  let max = worker2(arrOfArr[0]);
-
-  for ( let i = 0; i < arrOfArr.length; i++){
-    if (worker2(arrOfArr[i]) > max){
-      max = worker2(arrOfArr[i]);
-    }
-  }
-  return max;
+    return Math.abs(numMax-numMin);
 }
