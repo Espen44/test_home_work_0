030df1ea89cf3a1eea07ce74532e85d55bf0cdc0
Anton Savelev <maybesomedayilearn@gmail.com>
2022-07-19 13:57:58 +0300

    начал решать задачу

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,37 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 
 }
+  let stud1 = new Student('Anton', 'male', 40);
+  let stud2 = new Student('Andrey', 'male', 39);
+  let stud3 = new Student('Olga', 'female', 40);
+  
+  console.log(stud1, stud2, stud3);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+   Student.setSubject = 'Название предмета';
+  };
+stud1.setSubject = 'Algebra';
+stud2.setSubject = 'Russian Language';
+stud3.setSubject = 'Izo';
+
+console.log(stud1, stud2, stud3);
+
+
+
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
 }
+
+stud1.addMark(4,3,3);
+
+
+
 
 // ваш код для остальных методов
