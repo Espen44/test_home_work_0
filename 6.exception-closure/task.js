6de039a43511658caa0ec11f8cd65315d179bf4a
Espen <espen@espen.com>
2022-08-02 19:24:10 +0300

    Для проверки ДЗ

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,12 +1,11 @@
 function parseCount(value) {
     let number = Number.parseInt(value, 10);
-    if (isNaN(number)) {
-        const error = new Error("Невалидное значение");
-        throw error;
-    } 
+        if (isNaN(number)) {
+            const error = new Error('Невалидное значение');
+            throw error;
+        } 
     return number;
 }
-parseCount();
 
 function validateCount(value) {
     try {
@@ -15,21 +14,40 @@ function validateCount(value) {
         return error;
     }
 }
-validateCount();
 
-class Triangle (
+
+
+class Triangle {
     constructor(sideA, sideB, sideC){
-        let.sideA = sideA;
-        let.sideB = sideB;
-        let.sideC = sideC;
-        if ( sideA + sideB < sideC || sideA + sideC < sideB || sideB+sideC < sideA){
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+        if ( this.sideA + this.sideB < this.sideC || this.sideA + this.sideC < this.sideB || this.sideB+this.sideC < this.sideA){
             const error = new Error('Треугольник с такими сторонами не существует');
             throw error;
         }
+    }
+   
+    getPerimeter(){
+        let perimeter = this.sideA + this.sideB + this.sideC;
+        return +perimeter.toFixed(3);
+    }
 
-        getPerimeter(){
-            
+    getArea(){
+        let perimeter = this.sideA + this.sideB + this.sideC;
+        let poluperimeter = perimeter / 2;
+        let areaOfTreangle = Math.sqrt(poluperimeter*(poluperimeter - this.sideA)*(poluperimeter - this.sideB)*(poluperimeter - this.sideC));
+        return +areaOfTreangle.toFixed(3);
+    }
+}
+
+function getTriangle(sideA, sideB, sideC){
+    try {
+        return new Triangle(sideA, sideB, sideC);
+    } catch {
+        return {
+            getArea: () => "Ошибка! Треугольник не существует",
+            getPerimeter: () => "Ошибка! Треугольник не существует"
         }
     }
-)
->>>>>>> ba500f72658f6b19e294ac2586845a4071a29747
+}
