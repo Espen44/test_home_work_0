30949e2d9f05bbedfae0402aa0737a2bc467ce06
Anton Savelev <maybesomedayilearn@gmail.com>
2022-08-08 13:47:36 +0300

    начал решать

---
+++ 7.async/task.js
@@ -0,0 +1,17 @@
+class AlarmClock {
+    constructor(alarmCollection, timerId){
+        this.alarmCollection = [];
+        this.timerId = timerId;
+    }
+
+    addClock(time, callback, id){
+        if (id = undefined ){
+            throw new Error('error text');
+        }
+        if(id === id){
+            console.error();
+            return false;
+        }
+        this.alarmCollection.push(addClock);
+    }
+}
