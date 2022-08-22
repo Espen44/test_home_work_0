f9c3dc0c828e47b19c437532037b223492bf15c2
Anton Savelev <maybesomedayilearn@gmail.com>
2022-08-22 13:59:48 +0300

    делаю задание

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,17 +1,43 @@
 class AlarmClock {
     constructor(alarmCollection, timerId){
         this.alarmCollection = [];
-        this.timerId = timerId;
+        this.timerId = null;
     }
 
-    addClock(time, callback, id){
-        if (id = undefined ){
+    addClock( time, callback, id){
+        if (!id){
             throw new Error('error text');
         }
-        if(id === id){
-            console.error();
-            return false;
+        const findAlarm = this.alarmCollection.find((elem) => elem.id === id)
+        if(findAlarm) {
+            console.error('Будильник с таким id уже существует');
+            return;
         }
-        this.alarmCollection.push(addClock);
+        this.alarmCollection.push({time, callback, id});
     }
+
+    removeClock(id) {
+        const newAllarmCollection = this.alarmCollection.filter((alarm) => alarm.id !== id)       
+        if (this.alarmCollection.length === newAllarmCollection.length) {
+            return false;
+        } else {
+            this.alarmCollection = newAllarmCollection;
+            return true;
+        }
+    }
+
+    getCurrentFormattedTime(){
+       return new Date().toLocaleTimeString().slice(0,-3);
+    }
+
+    start() {
+        const checkClock = (alarm) => {
+            if( this.getCurrentFormattedTime() === alarm.time) {
+                
+            }
+         }
+    }
+    
 }
+
+
